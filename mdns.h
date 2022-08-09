96c61a237707e664d236c36faa53bbbbc2c647e6
Christian <76789692+ChristianHillenbrand@users.noreply.github.com>
2022-08-09 22:46:14 +0200

    parse boolean txt records (keys without value) correctly (#63)

--- mdns.h
+++ mdns.h
@@ -1572,11 +1572,13 @@ mdns_record_parse_txt(const void* buffer, size_t size, size_t offset, size_t len
 		++strdata;
 		offset += sublength + 1;
 
-		size_t separator = 0;
+		size_t separator = sublength;
 		for (size_t c = 0; c < sublength; ++c) {
 			// DNS-SD TXT record keys MUST be printable US-ASCII, [0x20, 0x7E]
-			if ((strdata[c] < 0x20) || (strdata[c] > 0x7E))
+			if ((strdata[c] < 0x20) || (strdata[c] > 0x7E)) {
+				separator = 0;
 				break;
+			}
 			if (strdata[c] == '=') {
 				separator = c;
 				break;
