da3528cdeae2d05ead0dc9359c5291998d752641
mb <murat@sunnyvalley.io>
2022-12-10 06:13:15 -0800

    Incorporate Mattias' suggestion.

--- mdns.h
+++ mdns.h
@@ -1569,11 +1569,14 @@ mdns_record_parse_txt(const void* buffer, size_t size, size_t offset, size_t len
 		strdata = (const char*)MDNS_POINTER_OFFSET(buffer, offset);
 		size_t sublength = *(const unsigned char*)strdata;
 
+		if (sublength >= (end - offset))
+			break;
+
 		++strdata;
 		offset += sublength + 1;
 
 		size_t separator = sublength;
-		for (size_t c = 0; c < sublength && ((strdata + c) < (char *)MDNS_POINTER_OFFSET(buffer, end)); ++c) {
+		for (size_t c = 0; c < sublength; ++c) {
 			// DNS-SD TXT record keys MUST be printable US-ASCII, [0x20, 0x7E]
 			if ((strdata[c] < 0x20) || (strdata[c] > 0x7E)) {
 				separator = 0;
